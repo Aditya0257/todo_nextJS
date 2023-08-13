de3150bc5b03198fb826b0a20e78742b665b53c6
Aditya0257 <adityasinghthakur.0257@gmail.com>
2023-08-13 10:48:36 +0530

    on click -> Home in header, it refreshes the task display of user, fixed bug with alternate method, making header.jsx as client component

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,15 +1,19 @@
+"use client"
+
 import Link from 'next/link';
 import React from 'react';
 import {LogoutBtn} from '../components/Clients.jsx';
+import { useRouter } from 'next/navigation';
 
 function Header() {
+  const router = useRouter();
   return (
     <div className='header'>
       <div>
         <h2>Todo.</h2>
       </div>
       <article>
-        <Link href='/'>Home</Link>
+        <Link href='/' onClick={()=>router.refresh()}>Home</Link>
         <Link href='/profile'>Profile</Link>
         
         <LogoutBtn />
